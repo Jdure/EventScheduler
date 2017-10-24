d10b966862d426803bd1b8a8aba1696c84819568
Robson Miranda <stra0165@algonquinlive.com>
2017-10-24 16:11:36 -0400

    Initial Commit

---
+++ EventScheduler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EventScheduler
+//
+//  Created by Robson Strabeli de Camargo Miranda on 2017-10-24.
+//  Copyright © 2017 stra015@algonquinlive.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
